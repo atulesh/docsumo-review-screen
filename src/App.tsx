3a483fa6678c78c5729aef4afc1590ed69e0e48d
atuleshkarna <atuleshkarna@gmail.com>
2025-04-03 19:16:53 +0545

    code formatter and tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ function App() {
 
   return (
     <>
-        {/* Start from here */}
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white">
+          <h1 className="text-4xl font-bold">Hello World</h1>
+        </div>
     </>
   )
 }
